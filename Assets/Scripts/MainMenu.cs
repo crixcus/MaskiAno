a2c82765a11c1cd933be62429fffdd6d14b7ad98
sui0809 <defaultzez@gmail.com>
2025-05-22 14:36:42 +0800

    inda mainmenu ni garo

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject instructionsPanel;
+    public GameObject settingsPanel;
+    public GameObject mainMenuButtons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instructionsPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+        mainMenuButtons.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("movement");
+    }
+
+    public void Settings()
+    {
+        settingsPanel.SetActive(true);
+        mainMenuButtons.SetActive(false);
+    }
+
+    public void Instructions()
+    {
+        instructionsPanel.SetActive(true);
+        mainMenuButtons.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void BackButton()
+    {
+        instructionsPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+        mainMenuButtons.SetActive(true);
+    }
+}
