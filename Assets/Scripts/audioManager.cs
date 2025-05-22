3b7f7c75f5f84905cda8a33b8d01ce4923d358aa
sui0809 <defaultzez@gmail.com>
2025-05-22 15:54:02 +0800

    mainmenu

---
+++ Assets/Scripts/audioManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class audioManager : MonoBehaviour
+{
+    public static audioManager Instance;
+
+    public Sound[] musicSound, sfxSound;
+    public AudioSource musicSource, sfxSource;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        PlayMusic("bgm2"); 
+    }
+
+    public void PlayMusic(string name)
+    {
+        Sound s = Array.Find(musicSound, x => x.name == name);
+
+        if (s == null)
+        {
+            Debug.LogError("Music not found: " + name);
+            return;
+        }
+
+        musicSource.clip = s.clip;
+        musicSource.loop = true;  // Ensure the music loops
+        musicSource.Play();  // This line was missing!
+    }
+
+    public void PlaySFX(string name)
+    {
+        Sound s = Array.Find(sfxSound, x => x.name == name);
+
+        if (s == null)
+        {
+            Debug.LogError("SFX not found: " + name);
+            return;
+        }
+
+        sfxSource.PlayOneShot(s.clip);
+    }
+}
